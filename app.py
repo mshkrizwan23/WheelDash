3f30fb36117748e1a9f7593b3357f8ed1aaefbfa
mshkrizwan23 <62351203+mshkrizwan23@users.noreply.github.com>
2025-04-22 12:25:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import dash
-from dash import html, dcc
+from dash import html
 import dash_bootstrap_components as dbc
 
+# Initialize app
 app = dash.Dash(
     __name__,
     use_pages=True,
+    suppress_callback_exceptions=True,
     external_stylesheets=[dbc.themes.BOOTSTRAP]
 )
 
 server = app.server
 
+# Main layout
 app.layout = dbc.Container([
     html.H2("🛠️ Freight Wheelset Maintenance Dashboard", className="mt-4"),
     dash.page_container
