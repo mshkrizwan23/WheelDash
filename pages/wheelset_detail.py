894af4d72abdb331b17c1876c554b9a07729e22d
mshkrizwan23 <62351203+mshkrizwan23@users.noreply.github.com>
2025-04-22 18:50:28 +0100

    Update wheelset_detail.py

--- pages/wheelset_detail.py
+++ pages/wheelset_detail.py
@@ -5,7 +5,7 @@ import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path_template="/wheelset/<wheelset_id>")
 
-condition_df = pd.read_csv("data/wheelset_condition_sample.csv")
+condition_df = pd.read_csv("data/wheelset_condition.csv")
 history_df = pd.read_csv("data/wheelset_history.csv")
 maint_df = pd.read_csv("data/dashboard_maintenance_records.csv")
 
