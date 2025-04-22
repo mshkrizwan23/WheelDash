7d63a47c373829e9930656f04ea77ebe0ea86a3b
mshkrizwan23 <62351203+mshkrizwan23@users.noreply.github.com>
2025-04-22 18:59:57 +0100

    Update wheelset_detail.py

--- pages/wheelset_detail.py
+++ pages/wheelset_detail.py
@@ -12,7 +12,7 @@ maint_df = pd.read_csv("data/dashboard_maintenance_records.csv")
 def layout(wheelset_id=None):
     wheel_info = condition_df[condition_df["Wheelset Serial"] == wheelset_id]
     maintenance = maint_df[maint_df["Wheelset Serial"] == wheelset_id]
-    history = history_df[history_df["Wheelset Serial Number"] == wheelset_id]
+    history = history_df[history_df["Wheelset Serial"] == wheelset_id]
 
     if wheel_info.empty:
         return html.H4("Wheelset not found.")
